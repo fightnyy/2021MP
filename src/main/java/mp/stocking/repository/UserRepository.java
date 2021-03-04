caf0b7bd7e92d584b60339f669a439b15647cd60
fightnyy <fightnyy@naver.com>
2021-03-04 23:12:45 +0900

    Login

---
+++ src/main/java/mp/stocking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package mp.stocking.repository;
+
+import mp.stocking.user.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByEmail(String email);
+}
---
+++ src/test/java/mp/stocking/StockingApplicationTests.java
@@ -0,0 +1,13 @@
+package mp.stocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
